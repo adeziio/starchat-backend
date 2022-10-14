1bff70052b3e5a9ba9f7269841724b739f292dbd
Aden <adeziio@yahoo.com>
2022-10-14 16:46:47 -0400

    comment out github service

--- src/utils/GithubService.py
+++ src/utils/GithubService.py
@@ -9,13 +9,14 @@ load_dotenv(find_dotenv())
 
 
 def pushToGithub():
-    root = str(os.path.join(loc, "./../.."))
-    repo = Repo(root)
-    serverName = str(os.path.join(loc, "./../..", ServerInfo.getServerName()))
-    files = [serverName]
-    commit_msg = "Automated Database Updates"
-    repo.index.add(files)
-    repo.index.commit(commit_msg)
-    origin = repo.remote('origin')
-    origin.pull()
-    origin.push().raise_if_error()
+    # root = str(os.path.join(loc, "./../.."))
+    # repo = Repo(root)
+    # serverName = str(os.path.join(loc, "./../..", ServerInfo.getServerName()))
+    # files = [serverName]
+    # commit_msg = "Automated Database Updates"
+    # repo.index.add(files)
+    # repo.index.commit(commit_msg)
+    # origin = repo.remote('origin')
+    # origin.pull()
+    # origin.push().raise_if_error()
+    None
