9a33f221ae9f33bfe0bc493b77bb45fec8bcd133
Aden <adeziio@yahoo.com>
2022-10-19 01:37:14 -0400

    update aws config env names

--- src/configs/AWSConfig.py
+++ src/configs/AWSConfig.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_REGION_NAME = os.getenv("AWS_REGION_NAME")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID_CONFIG")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY_CONFIG")
+AWS_REGION_NAME = os.getenv("AWS_REGION_NAME_CONFIG")
